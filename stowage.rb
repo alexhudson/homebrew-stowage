e0c4a7a0a4e3d2591806f9c5bacd591505bf6d24
Alex Hudson <ealexhudson@gmail.com>
2017-09-03 10:24:03 +0100

    Update stowage.rb
    Manually updated again; goreleaser config is a bit wrong! D'oh.

--- stowage.rb
+++ stowage.rb
@@ -1,11 +1,15 @@
 class Stowage < Formula
   desc "Package manager-alike for wrapping Docker containers as binaries"
-  homepage "https://github.com/alexhudson/stowage"
-  url "https://github.com/alexhudson/stowage/releases/download/0.1.1/stowage_0.1.1_macOS_64-bit.tar.gz"
-  version "v0.1.1"
-  sha256 "3b249af1b33486b3c262511c2caa4569ed3eb155579e0d1c59117c291b0288c7"
+  homepage "https://stowage.org/"
+  url "https://github.com/alexhudson/stowage/releases/download/0.2.0/stowage_0.2.0_macOS_64-bit.tar.gz"
+  version "0.2.0"
+  sha256 "ad957dd89110056e874352d26136c675ab9daf0282a04798c0336d7136edcf30"
 
   def install
     bin.install "stowage"
+  end
+
+  test do
+    
   end
 end
