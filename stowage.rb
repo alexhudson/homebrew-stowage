d11f9ce60edd0a8ed257f51e408f41dd880f41a3
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-06 22:12:43 +0100

    stowage version 0.3.0

--- stowage.rb
+++ stowage.rb
@@ -1,9 +1,9 @@
 class Stowage < Formula
   desc "Package manager-alike for wrapping Docker containers as binaries"
   homepage "https://stowage.org/"
-  url "https://github.com/alexhudson/stowage/releases/download/0.2.0/stowage_0.2.0_macOS_64-bit.tar.gz"
-  version "0.2.0"
-  sha256 "ad957dd89110056e874352d26136c675ab9daf0282a04798c0336d7136edcf30"
+  url "https://github.com/alexhudson/stowage/releases/download/0.3.0/stowage_0.3.0_macOS_64-bit.tar.gz"
+  version "0.3.0"
+  sha256 "ce7b6a8f748074fb4d253d58adcc6c6681da53b411a568f95ed82d2ef4e4cde1"
 
   def install
     bin.install "stowage"
